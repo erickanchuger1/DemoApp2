1b4f1c2ccd3d8c0bae698f462cf9cf0f54fb9f10
erickanchuger1 <ekanchu@amazon.com>
2025-04-23 20:16:51 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="welcome-container">
+      <h1>Welcome Eric Kanchuger</h1>
+      <p>You made something!</p>
+    </div>
+  );
+}
+
+export default App;
